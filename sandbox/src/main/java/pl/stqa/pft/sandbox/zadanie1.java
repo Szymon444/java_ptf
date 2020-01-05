126edd3567aaa6e8f65a289231cec4ba816212a2
Slide2020 <59455702+Slide2020@users.noreply.github.com>
2020-01-05 10:25:04 +0100

    wprowadzenie metod

--- sandbox/src/main/java/pl/stqa/pft/sandbox/zadanie1.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/zadanie1.java
@@ -8,20 +8,14 @@ public class Zadanie1 {
 			hello("Sim");
 
 			Square s = new Square (5);
-			System.out.println("Powierzchnia kwadratu z bokiem o długości " + s.l + " = " + area(s) );
+			System.out.println("Powierzchnia kwadratu z bokiem o długości " + s.l + " = " + s.area());
 
 			Rectangle r = new Rectangle (4, 6);
-			System.out.println("Powierzchnia prostokątu z bokami o długościach  " + r.a + " i " + r.b + " = " + area(r) );
+			System.out.println("Powierzchnia prostokątu z bokami o długościach  " + r.a + " i " + r.b + " = " + r.area());
 		}
 		public static void hello (String somebody) {
 			System.out.println("Hello " + somebody);
 		}
 
-		public static double area(Square s) {
-			return s.l * s.l;
-		}
-		public static double area (Rectangle r) {
-			return r.a * r.b;
-		}
 	}
 
