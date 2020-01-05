5899c03cf822bb9e9c3572294c2996d8618fe23c
Slide2020 <59455702+Slide2020@users.noreply.github.com>
2020-01-05 09:23:17 +0100

    dodane parę funkcji

--- sandbox/src/main/java/pl/stqa/pft/sandbox/zadanie1.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/zadanie1.java
@@ -1,7 +1,28 @@
 package pl.stqa.pft.sandbox;
 
 public class zadanie1 {
-	public static void main(String[] args) {
-		System.out.println("It's my first homework!");
+
+		public static void main (String[] args) {
+
+			hello("world");
+			hello("Sim");
+
+			double len = 5;
+			double a = 4;
+			double b = 6;
+			System.out.println("Powierzchnia kwadratu z bokiem o długości " + len + " = " + area(len) );
+
+			System.out.println("Powierzchnia prostokątu z bokami o długościach  " + a + " i " + b + " = " + area(a, b) );
+		}
+		public static void hello (String somebody) {
+			System.out.println("Hello " + somebody);
+		}
+
+		public static double area(double l) {
+			return l * l;
+		}
+		public static double area (double a, double b) {
+			return a * b;
+		}
 	}
-}
+
